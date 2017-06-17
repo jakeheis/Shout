79ee4dcb104adc97f80ca27d59693639f09348f3
Jake Heiser <jakeheiser1@gmail.com>
2017-06-17 09:56:55 -0500

    Use better cssh package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Shout",
     dependencies: [
-        .Package(url: "https://github.com/jakeheis/CSSH", majorVersion: 1),
+        .Package(url: "https://github.com/alexaubry/cssh", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/BlueSocket", majorVersion: 0)
     ]
 )
