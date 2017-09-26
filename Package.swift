848a3e3f5a5e6f7396bc5708585b2d8c01c73efa
Jake Heiser <jakeheiser1@gmail.com>
2017-09-26 15:49:53 -0700

    Upgrade to Swift 4 and fix missing dependency

--- Package.swift
+++ Package.swift
@@ -1,11 +1,17 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "Shout",
+    products: [
+        .library(name: "Shout", targets: ["Shout"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/alexaubry/cssh", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket", majorVersion: 0)
+        .package(url: "https://github.com/jakeheis/CSSH", from: "1.0.3"),
+        .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "0.12.68")
+    ],
+    targets: [
+        .target(name: "Shout", dependencies: ["Socket"])
     ]
 )
